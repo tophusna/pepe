60256109224adc8efbac9fe4ecb2ff988102b533
tophusna <153291727+tophusna@users.noreply.github.com>
2024-06-03 02:06:19 -0400

    aaa

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,48 +1,87 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const images = [
-  '/images/pepe1.png',
-  '/images/pepe2.png',
-  
+  "/images/pepe3.webp",
+  "/images/pepe3.webp",
+  "/images/pepe4.webp",
+  "/images/pepe4.webp",
+  "/images/pepe5.webp",
+  "/images/pepe5.webp",
   // Add more image paths as needed
 ];
 
-const LoadingScreen = (props) => {
+const LoadingScreen = ({ handleClick }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [progress, setProgress] = useState(0);
-  const handleClick = props.handleClick;
+  const [fade, setFade] = useState("animate-fadeIn"); // State to manage fade animation class
+  const [soundtrack] = useState(new Audio("/asset/musics/mp3.mp3")); // State to manage soundtrack
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-      setProgress((prevProgress) => {
-        const newProgress = prevProgress + 1;
-        if (newProgress >= 5) {
-          clearInterval(interval); // Stop the interval when progress reaches 10
-        }
-        return newProgress;
+    const playSoundtrack = () => {
+      soundtrack.play().catch(error => {
+        console.log('Autoplay was prevented:', error);
       });
-    }, 2000); // Change image and update progress every second
+    };
+
+    playSoundtrack(); // Attempt to play the soundtrack when component mounts
+
+    return () => {
+      soundtrack.pause(); // Pause the soundtrack when component unmounts
+      soundtrack.currentTime = 0; // Reset playback to the start
+    };
+  }, [soundtrack]);
+
+  console.log("currentImageIndex", currentImageIndex);
+  console.log("fade", fade);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => {
+        setCurrentImageIndex((prevIndex) => prevIndex + 1);
+        currentImageIndex % 2 === 0 && setFade("animate-fadeOut");
+        currentImageIndex % 2 === 1 && setFade("animate-fadeIn");
+        // setTimeout(() => setFade('hidden'), 3000); // Hide after 3 seconds`````````````
+
+        setProgress((prevProgress) => {
+          const newProgress = prevProgress + 1;
+          if (newProgress >= 5) {
+            clearInterval(interval); // Stop the interval when progress reaches 5
+          }
+          return newProgress;
+        });
+      },
+      currentImageIndex < 5 ? 3000 : 9999999999
+    ); // Change image and update progress every 2 seconds until currentImageIndex reaches 10
 
     return () => clearInterval(interval);
-  }, []);
+  }, [currentImageIndex]);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
-      <div className="relative h-screen">
-        <img src={images[currentImageIndex]} alt="San Andreas Logo" className={`loading-logo h-full transition-opacity duration-1000 animate-fadein`} />
+
+      <div className="relative h-screen w-screen">
+        <img
+          key={currentImageIndex}
+          src={images[currentImageIndex]}
+          alt="San Andreas Logo"
+          className={`h-full w-full object-cover ${fade}`}
+        />
         <div className="absolute bottom-20 left-20 w-[200px] h-2 bg-gray-700">
           {progress < 6 && (
             <div
-              className="h-full bg-green-500 transition-all duration-2000"
+              className="h-full bg-green-500 transition-all duration-[2000ms]"
               style={{ width: `${progress * 20}%` }}
             ></div>
           )}
           {progress >= 5 && (
-          <button className="absolute bottom-[0px] left-1/2 transform -translate-x-1/2 -translate-y-1/2  rounded-md text-white" onClick={handleClick}>Click here</button>
-        )}
+            <button
+              className="absolute bottom-[10px] left-[15px] text-white"
+              onClick={handleClick}
+            >
+              Click here
+            </button>
+          )}
         </div>
-        
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     extend: {
       backgroundImage: {
         'landing1': "url('/img/land1_1.png)"
-      }
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        fadeOut: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+        fadeOut: 'fadeOut 1s ease-in-out 1s forwards',
+      },
     },
   },
   plugins: [],
